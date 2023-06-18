b848ccb80782ca50e3e9ad55b1c35fe47c83e361
Nilton Andrade <nilton.andrade@ymail.com>
2023-06-18 02:56:53 -0300

    Create ambiente.js
    Aula 13 - Parte 01

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+     do {
+        console.log(`Passo ${c}`)
+        c++
+    } while (c <= 6)
+
+
+
+    // Este modelo é o mais usado atualmente.
+/* 
+var c = 1
+    while (c <= 6) {
+        console.log(`Passo ${c}`)
+        c++
+    } */
+
+
+    //Este modelo é antigo e obsoleto.
+/*
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+*/    
